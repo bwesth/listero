2fdc4a249aa8cc70893af8e7b7794fc4cc9b984e
bwesth <bjwe@itu.dk>
2023-03-27 15:19:07 +0200

    Trin 12.9

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,12 @@
 function List() {
     
     let list = ["Lav komponent", "brug states", "brug iterativ rendering", "brug conditional rendering", "brug propts"]
-  
-    function parseToListItem(item) {
-        return <li>{item}</li>
-    }
 
     return (
     <div>
       <h1>Min Liste</h1>
       <ul>
-        {list.map(parseToListItem)}
+        {list.map(item => <li>{item}</li>)}
       </ul>
     </div>
   );
