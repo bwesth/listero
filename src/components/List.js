e70296df989c03c21b2adb5a1fae141718a998d5
bwesth <bjwe@itu.dk>
2023-03-27 15:21:09 +0200

    Trin 13

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,27 @@
 function List() {
-    
-    let list = ["Lav komponent", "brug states", "brug iterativ rendering", "brug conditional rendering", "brug propts"]
+//   let list = [
+//     "Lav komponent",
+//     "brug states",
+//     "brug iterativ rendering",
+//     "brug conditional rendering",
+//     "brug propts",
+//   ];
 
-    return (
-    <div>
-      <h1>Min Liste</h1>
-      <ul>
-        {list.map(item => <li>{item}</li>)}
-      </ul>
-    </div>
+  let list = [
+    
+  ];
+
+  return (
+    list.length > 0 && (
+      <div>
+        <h1>Min Liste</h1>
+        <ul>
+          {list.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    )
   );
 }
 
