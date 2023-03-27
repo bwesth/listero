74c17ebed2dcbfae7ad4e8d1ce968e7d8a8d8655
bwesth <bjwe@itu.dk>
2023-03-27 15:10:52 +0200

    Trin 12.4

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,12 @@
 function List() {
-  return (
+    
+    let list = ["Lav komponent", "brug states", "brug iterativ rendering", "brug conditional rendering", "brug propts"]
+  
+    return (
     <div>
       <h1>Min Liste</h1>
       <ul>
-        
+
       </ul>
     </div>
   );
