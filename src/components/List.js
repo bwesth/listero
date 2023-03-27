b912fe15586ca157c8f4e3c144da023adcb97d6a
bwesth <bjwe@itu.dk>
2023-03-27 15:33:38 +0200

    Trin 15.3

--- src/components/List.js
+++ src/components/List.js
@@ -13,6 +13,7 @@ function List() {
   return (
     <div>
       <h1>Min Liste</h1>
+      <input type="text" value={input}/>
       {list.length > 0 ? (
         <ul>
           {list.map((item) => (
